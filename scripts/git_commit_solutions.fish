943c67f5eddedc0c61d637ea95771fd62010ccfc
V K <84337571+1e9y@users.noreply.github.com>
2022-10-06 21:39:55 +0200

    feat: add script for generating commit messages

---
+++ scripts/git_commit_solutions.fish
@@ -0,0 +1,33 @@
+#!/usr/bin/env fish
+
+function clean_commit -d "Creates clean commit messages"
+  set directory "."
+
+  for d in (find -E $directory -type d -maxdepth 1 -regex ".*[0-9]{4}\.[a-z_]+.*")
+    check_changes $d
+  end
+end
+
+function check_changes
+  set path $argv[1]
+
+  if not git diff --quiet $path
+    set message (get_commit_message $path)
+    pushd $path
+    git add .
+    git commit --message $message
+    popd
+  end
+end  
+
+function get_commit_message
+  set dirname (basename $argv[1])
+
+  set parts (string match --regex '([0-9]+)\.([a-z_]+)' $dirname)
+  set number (string replace --regex '^0+' '' $parts[2])
+  set name (string replace --all '_' ' ' $parts[3])
+  set message "solution($number): $name"
+  echo $message
+end
+
+clean_commit
